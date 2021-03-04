9b652e99cfbe54f7bd674cc242b43c8888835d19
UT0903 <b07902048@ntu.edu.tw>
2021-03-04 20:05:10 +0800

    not recv yet

--- hw1/traceroute_samplecode.c
+++ hw1/traceroute_samplecode.c
@@ -11,13 +11,40 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 #include<sys/time.h>
+#include<iostream>
 
 char *DNSLookup(char *host){
-    // TODO
+    struct hostent *ghbn = gethostbyname(host);//change the domain name
+    //printf("in\n");
+    if (!ghbn) {
+        fprintf(stderr, "Can't recolve host name\n");
+        exit(1);
+    }
+    return (char *)inet_ntoa(*(struct in_addr *)ghbn->h_addr);
 }
+
+unsigned short checksum(unsigned short *buf, int bufsz){
+    unsigned long sum = 0xffff;
+
+    while(bufsz > 1){
+        sum += *buf;
+        buf++;
+        bufsz -= 2;
+    }
+
+    if(bufsz == 1)
+        sum += *(unsigned char*)buf;
+
+    sum = (sum & 0xffff) + (sum >> 16);
+    sum = (sum & 0xffff) + (sum >> 16);
+
+    return ~sum;
+}
+
 
 int main(int argc, char *argv[]){
     char *dest = argv[1];
+    //fprintf(stderr, "%s\n", argv[1]);
     char *ip = DNSLookup(dest);
     if(ip == NULL){
         printf("traceroute: unknown host %s\n", dest);
@@ -25,17 +52,29 @@ int main(int argc, char *argv[]){
     }
     int icmpfd;
     if((icmpfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0){
-        printf("Can not open socket with error number %d\n", errno);
+        printf("Can not open socket\n");
         exit(1);
     }
     
-    struct sockaddr_in sendAddr;
+    struct sockaddr_in sendAddr, recvAddr;
     sendAddr.sin_port = htons (7);
     sendAddr.sin_family = AF_INET;
     inet_pton(AF_INET, ip, &(sendAddr.sin_addr));
     
     // Set timeout
     // TODO
+    struct timeval timeout;      
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+
+    if (setsockopt (icmpfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+                sizeof(timeout)) < 0)
+        perror("setsockopt failed\n");
+
+    if (setsockopt (icmpfd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
+                sizeof(timeout)) < 0)
+        perror("setsockopt failed\n");
+
 
     int finish = 0; // if the packet reaches the destination
     int maxHop = 64; // maximum hops
@@ -44,32 +83,47 @@ int main(int argc, char *argv[]){
     int seq = 0; // increasing sequence number for icmp packet
     int count = 3; // sending count for each ttl
     printf("traceroute to %s (%s), %d hops max\n", dest, ip, maxHop);
+    memset(&sendICMP, 0, sizeof(sendICMP));
     for(int h = 1; h < maxHop; h++){
         // Set TTL
         // TODO
-
+        setsockopt(icmpfd, IPPROTO_IP, IP_TTL, &h, sizeof(h));
+        //fprintf(stderr, "ok");
         for(int c = 0; c < count; c++){
             // Set ICMP Header
             // TODO
-
+            sendICMP.icmp_code = 0;
+            sendICMP.icmp_type = ICMP_ECHO;
+            sendICMP.icmp_hun.ih_idseq.icd_id = 5566;
+            sendICMP.icmp_hun.ih_idseq.icd_seq = seq;
             // Checksum
             // TODO
-            
+            sendICMP.icmp_cksum = checksum((unsigned short *)&sendICMP, sizeof(sendICMP));
             // Send the icmp packet to destination
             // TODO
-        
+            gettimeofday(&begin, NULL);
+            sendto(icmpfd, (char*)&sendICMP, sizeof(sendICMP), 0, (const struct sockaddr *)&sendAddr, sizeof(sendAddr));
+            
             // Recive ICMP reply, need to check the identifier and sequence number
             struct ip *recvIP;
             struct icmp *recvICMP;
-            struct sockaddr_in recvAddr;
+            
             u_int8_t icmpType;
             unsigned int recvLength = sizeof(recvAddr);
             char recvBuf[1500];
             char hostname[4][128];
             char srcIP[4][32];
             float interval[4] = {};
-            memset(&recvAddr, 0, sizeof(struct sockaddr_in));
             // TODO
+            memset(&recvAddr, 0, sizeof(struct sockaddr_in));
+            // recvfrom(icmpfd, recvBuf, sizeof(recvBuf), 0, &recvAddr, recvLength);
+            // recvIP = (struct ip *)recvBuf;
+            // recvICMP = (struct icmp *) (recvBuf + recvIP->ip_hl * 4);
+            // icmpType = recvICMP -> icmp_type;
+            // if(icmpType == ICMP_TIMXCEED) {
+            //     printf("TimeOut\n");
+            //     continue;
+            // }
 
             // Get source hostname and ip address 
             getnameinfo((struct sockaddr *)&recvAddr, sizeof(recvAddr), hostname[c], sizeof(hostname[c]), NULL, 0, 0); 
