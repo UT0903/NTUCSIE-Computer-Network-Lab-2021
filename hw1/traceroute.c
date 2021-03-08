d03cc0bfad5443ef28d49233afbd06a6f4241d6b
UT0903 <b07902048@ntu.edu.tw>
2021-03-08 20:38:51 +0800

    too much sned

--- hw1/traceroute.c
+++ hw1/traceroute.c
@@ -94,79 +94,80 @@ int main(int argc, char *argv[]){
         if (setsockopt (icmpfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
                     sizeof(timeout)) < 0)
             perror("setsockopt failed\n");
-    int finish = 0; // if the packet reaches the destination
-    int maxHop = 64; // maximum hops
-    struct icmp sendICMP; 
-    struct timeval begin, end; // used to record RTT
-    int seq = 0; // increasing sequence number for icmp packet
-    int count = 3; // sending count for each ttl
-    printf("traceroute to %s (%s), %d hops max\n", dest, ip, maxHop);
-    memset(&sendICMP, 0, sizeof(sendICMP));
-    for(int h = 1; h < maxHop; h++){
-        setsockopt(icmpfd, IPPROTO_IP, IP_TTL, &h, sizeof(h));
-        //fprintf(stderr, "ok");
-        char srcIP[count][32];
-        char hostname[count][128];
-        int usec_info[count];
-        for(int c = 0; c < count; c++){
-            // Set ICMP Header
-            // TODO
-            sendICMP.icmp_code = 0;
-            sendICMP.icmp_type = ICMP_ECHO;
-            sendICMP.icmp_hun.ih_idseq.icd_id = 5566;
-            sendICMP.icmp_hun.ih_idseq.icd_seq = seq;
-            // Checksum
-            // TODO
-            sendICMP.icmp_cksum = checksum((unsigned short *)&sendICMP, sizeof(sendICMP));
-            // Send the icmp packet to destination
-            // TODO
-            gettimeofday(&begin, NULL);
-            sendto(icmpfd, (char*)&sendICMP, sizeof(sendICMP), 0, (const struct sockaddr *)&sendAddr, sizeof(sendAddr));
-            fprintf(stderr, "send hop: %d, c: %d\n", h, c+1);
-            // Recive ICMP reply, need to check the identifier and sequence number
-            struct ip *recvIP;
-            struct icmp *recvICMP;
-            
-            u_int8_t icmpType;
-            char recvBuf[1500];
-            
-            
-            float interval[4] = {};
-            // TODO
-            memset(&recvAddr, 0, sizeof(struct sockaddr_in));
-            recvfrom(icmpfd, recvBuf, sizeof(recvBuf), 0, &recvAddr, sizeof(recvAddr));
-            fprintf(stderr, "recv hop: %d, c: %d\n", h, c+1);
-            recvIP = (struct ip *)recvBuf;
-            recvICMP = (struct icmp *) (recvBuf + recvIP->ip_hl * 4);
-            icmpType = recvICMP -> icmp_type;
-            if(icmpType == ICMP_TIMXCEED) {
-                //printf("TimeOut\n");
-                gettimeofday(&end, NULL);
-                usec_info[c] = (end.tv_sec - begin.tv_sec)*1000000 + (end.tv_usec - begin.tv_usec);
+        int finish = 0; // if the packet reaches the destination
+        int maxHop = 64; // maximum hops
+        struct icmp sendICMP; 
+        struct timeval begin, end; // used to record RTT
+        int seq = 0; // increasing sequence number for icmp packet
+        int count = 3; // sending count for each ttl
+        printf("traceroute to %s (%s), %d hops max\n", dest, ip, maxHop);
+        memset(&sendICMP, 0, sizeof(sendICMP));
+        for(int h = 1; h < maxHop; h++){
+            setsockopt(icmpfd, IPPROTO_IP, IP_TTL, &h, sizeof(h));
+            //fprintf(stderr, "ok");
+            char srcIP[count][32];
+            char hostname[count][128];
+            int usec_info[count];
+            for(int c = 0; c < count; c++){
+                // Set ICMP Header
+                // TODO
+                sendICMP.icmp_code = 0;
+                sendICMP.icmp_type = ICMP_ECHO;
+                sendICMP.icmp_hun.ih_idseq.icd_id = 5566;
+                sendICMP.icmp_hun.ih_idseq.icd_seq = seq;
+                // Checksum
+                // TODO
+                sendICMP.icmp_cksum = checksum((unsigned short *)&sendICMP, sizeof(sendICMP));
+                // Send the icmp packet to destination
+                // TODO
+                gettimeofday(&begin, NULL);
+                sendto(icmpfd, (char*)&sendICMP, sizeof(sendICMP), 0, (const struct sockaddr *)&sendAddr, sizeof(sendAddr));
+                fprintf(stderr, "send hop: %d, c: %d\n", h, c+1);
+                // Recive ICMP reply, need to check the identifier and sequence number
+                struct ip *recvIP;
+                struct icmp *recvICMP;
+                
+                u_int8_t icmpType;
+                char recvBuf[1500];
+                
+                
+                float interval[4] = {};
+                // TODO
+                memset(&recvAddr, 0, sizeof(struct sockaddr_in));
+                recvfrom(icmpfd, recvBuf, sizeof(recvBuf), 0, &recvAddr, sizeof(recvAddr));
+                fprintf(stderr, "recv hop: %d, c: %d\n", h, c+1);
+                recvIP = (struct ip *)recvBuf;
+                recvICMP = (struct icmp *) (recvBuf + recvIP->ip_hl * 4);
+                icmpType = recvICMP -> icmp_type;
+                if(icmpType == ICMP_TIMXCEED) {
+                    //printf("TimeOut\n");
+                    gettimeofday(&end, NULL);
+                    usec_info[c] = (end.tv_sec - begin.tv_sec)*1000000 + (end.tv_usec - begin.tv_usec);
+                }
+
+                // Get source hostname and ip address 
+                int ret = 0;
+                if(ret = getnameinfo((struct sockaddr *)&recvAddr, sizeof(recvAddr), (char *)&hostname[c], sizeof(hostname[c]), NULL, 0, 0) < 0){
+                    //fprintf(stderr, "ret error\n");
+                    strcpy(hostname[c], "no");
+                }
+                strcpy(srcIP[c], inet_ntoa(recvIP->ip_src));
+                if(icmpType == ICMP_ECHOREPLY){
+                    printf("end\n");
+                    finish = 1;
+                }
+                else if (icmpType == ICMP_UNREACH) {
+                    printf("Unreachable\n");
+                }
             }
 
-            // Get source hostname and ip address 
-            int ret = 0;
-            if(ret = getnameinfo((struct sockaddr *)&recvAddr, sizeof(recvAddr), (char *)&hostname[c], sizeof(hostname[c]), NULL, 0, 0) < 0){
-                //fprintf(stderr, "ret error\n");
-                strcpy(hostname[c], "no");
+            Print_Format(h, hostname, srcIP, usec_info);
+
+            if(finish){
+                break;
             }
-            strcpy(srcIP[c], inet_ntoa(recvIP->ip_src));
-            if(icmpType == ICMP_ECHOREPLY){
-                printf("end\n");
-                finish = 1;
-            }
-            else if (icmpType == ICMP_UNREACH) {
-                printf("Unreachable\n");
-            }
+            close(icmpfd);
         }
-
-        Print_Format(h, hostname, srcIP, usec_info);
-
-        if(finish){
-            break;
-        }
-        close(icmpfd);
     }
     return 0;
 }
